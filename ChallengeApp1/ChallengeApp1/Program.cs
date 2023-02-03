059616e41859f4dace5c7ed1d2f193910b5a2976
Tomasz <tomaszkluska@aol.co.uk>
2023-02-03 23:49:25 +0000

    Dzien -4

--- ChallengeApp1/ChallengeApp1/Program.cs
+++ ChallengeApp1/ChallengeApp1/Program.cs
@@ -1,2 +1,21 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿//zadanie domowe-dzien4
+var name = "Ewa";
+bool female = true;
+var age = 33;
+
+if (female==true && age<30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name=="Ewa" && age==33)
+{
+    Console.WriteLine("Ewa,lat 33");
+}
+if (female != true && age < 18)
+{
+    Console.WriteLine("Niepelnoletni Mezczyzna");
+}
+
+
+
+
