7e0bb418f0310c5c0fd08c7dd034c6c40286eb5c
Tomasz <tomaszkluska@aol.co.uk>
2023-02-04 01:23:11 +0000

    zadanie-dzien5

--- ChallengeApp1/ChallengeApp1/Program.cs
+++ ChallengeApp1/ChallengeApp1/Program.cs
@@ -1,21 +1,71 @@
-﻿//zadanie domowe-dzien4
-var name = "Ewa";
-bool female = true;
-var age = 33;
+﻿int number = 459431;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToArray();
 
-if (female==true && age<30)
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+foreach (char letter in letters)
 {
-    Console.WriteLine("Kobieta ponizej 30 lat");
-}
-else if (name=="Ewa" && age==33)
-{
-    Console.WriteLine("Ewa,lat 33");
-}
-if (female != true && age < 18)
-{
-    Console.WriteLine("Niepelnoletni Mezczyzna");
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
 }
 
-
-
+Console.WriteLine("skladnik liczby to:" + number);
+Console.WriteLine("0 jest" + counter0);
+Console.WriteLine("1 jest" + counter1);
+Console.WriteLine("2 jest" + counter2);
+Console.WriteLine("3 jest" + counter3);
+Console.WriteLine("4 jest" + counter4);
+Console.WriteLine("5 jest" + counter5);
+Console.WriteLine("6 jest" + counter6);
+Console.WriteLine("7 jest" + counter7);
+Console.WriteLine("8 jest" + counter8);
+Console.WriteLine("9 jest" + counter9);
 
